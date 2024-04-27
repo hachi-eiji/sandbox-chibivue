cb607f30e5c54fb21548caebbe47edc563f9bf34
Eiji Hachiya <hachiyae@gmail.com>
2024-04-27 23:56:46 +0900

    feat: add setup function

--- examples/playground/src/main.ts
+++ examples/playground/src/main.ts
@@ -1,16 +1,18 @@
 import {createApp, h} from 'chibivue'
 
 const app = createApp({
-  render() {
-    return h('div', {id: 'my-app'}, [
-      h('p', {style: 'color:red; font-weight: bold;'} ,['Hello world']),
-      h('button', {
-        onClick() {
-          alert('hello')
-        }
-      } ,['click'])
-    ]
-    );
+  setup() {
+
+    return function render(){
+      return h('div', {id: 'my-app'}, [
+        h('p', {style: 'color:red; font-weight: bold;'} ,['Hello world']),
+        h('button', {
+          onClick() {
+            alert('hello')
+          }
+        } ,['click'])
+      ]);
+    }
   }
 })
 app.mount('#app')
